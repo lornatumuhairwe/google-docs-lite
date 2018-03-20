2e59bcdb9f5a108d53b45e02bec21c4a704100f2
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2018-03-20 22:51:58 +0300

    setup frontend backend comms

--- docs-frontend/src/App.js
+++ docs-frontend/src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { text: '' };
+
+  componentDidMount(){
+      window.fetch('http://localhost:3001/notes/1').then(data => {
+          data.json().then(res => {
+              this.setState({ text: res.text })
+          })
+      })
+  }
+
+  handleChange = e => {
+      this.setState({ text: e.target.value })
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <textarea
+          value={this.state.text}
+          onChange={this.handleChange}
+      />
     );
   }
 }
